c8b99a86cc857c41e797f46b994c0033e68bafd4
millena5444 <marco1264989898@gmail.com>
2025-03-13 20:54:50 -0300

    Update ex-05.py

--- ex-05.py
+++ ex-05.py
@@ -1,1 +1,7 @@
-
+#5. Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+print("aluno (A)   nota")
+print("=========   =====")
+print("ALINE       9.0")
+print("MÁRIO       DEZ")
+print("SÉRGIO      4.5")
+print("SHIRLEY     7.0")
