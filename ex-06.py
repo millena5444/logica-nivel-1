128eeb187e43d2ebc3c0618aee85d3320c5bab9e
millena5444 <marco1264989898@gmail.com>
2025-03-13 20:58:35 -0300

    ex-06.py

--- ex-06.py
+++ ex-06.py
@@ -1,1 +1,8 @@
-
+#6. Menu - Elabore um programa que mostre o seguinte menu na tela:
+print("cadastro de clientes")
+print("0 - fim")
+print("1 - inclui")
+print("2 - altera")
+print("3 - Exclui")
+print("4 - Consulta")
+print("opção:")
